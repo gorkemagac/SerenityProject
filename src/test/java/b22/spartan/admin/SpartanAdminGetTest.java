3277185e526833dd037c5c501abc2bc18a1249e4
Gorkem Agac <gorkem_agac@hotmail.com>
2023-08-30 18:17:43 +0100

    Serenity

---
+++ src/test/java/b22/spartan/admin/SpartanAdminGetTest.java
@@ -0,0 +1,95 @@
+package b22.spartan.admin;
+
+
+import io.restassured.http.*;
+import net.serenitybdd.junit5.*;
+import net.serenitybdd.rest.*;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.*;
+import static net.serenitybdd.rest.SerenityRest.given;
+import static net.serenitybdd.rest.SerenityRest.*;
+import static org.hamcrest.Matchers.*;
+
+//@Disabled
+@SerenityTest
+public class SpartanAdminGetTest {
+
+    @BeforeAll
+    public static void setUpBase() {
+        baseURI = "http://3.216.30.92:7000";
+    }
+
+
+
+    @Test
+    public void getAllSpartan(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .when()
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON)
+                .extract().response();
+    }
+
+    @Test
+    public void getOneSpartan(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .pathParam("id",15)
+                .when()
+                .get("/api/spartans/{id}");
+
+        //if you send a request using SerenityRest, the response object
+        //can be obtained from the method called lastResponse() without being saved separately
+        //same with Response response object
+        System.out.println("Status code = " + SerenityRest.lastResponse().statusCode());
+
+        //print id
+        //instead of response.path, we use lastResponse.path()
+        System.out.println("lastResponse().path(\"id\") = " + lastResponse().path("id"));
+
+        //use jsonpath with lastResponse and get the name
+        String name = lastResponse().jsonPath().getString("name");
+        System.out.println("name = " + name);
+    }
+
+
+    @DisplayName("GET request with Serenity Assertion way")
+    @Test
+    public void getOneSpartanAssertion(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .pathParam("id",15)
+                .when()
+                .get("/api/spartans/{id}");
+
+        //Serenity way of assertion
+
+        Ensure.that("Status code is 200",validatableResponse -> validatableResponse.statusCode(200) );
+
+        Ensure.that("Content-type is JSON",vRes -> vRes.contentType(ContentType.JSON));
+
+        Ensure.that("Id is 15", vRes -> vRes.body("id",is(15)));
+
+
+
+
+
+
+    }
+
+
+}
